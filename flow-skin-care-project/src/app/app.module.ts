bbfb31daca37b22d5894c75563945a98b623b8a1
Alaa <alaa.kortoma@vodafone.com>
2021-08-31 23:48:52 +0200

    DEVELOP demo 1

--- flow-skin-care-project/src/app/app.module.ts
+++ flow-skin-care-project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogoNavbarComponent } from './logo-navbar/logo-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoNavbarComponent
   ],
   imports: [
     BrowserModule,
